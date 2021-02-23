c33b5b0722a922beb8b8ecfc52f8613e2d41058c
tomsonas3000 <tomasjorudas23@gmail.com>
2021-02-23 15:16:07 +0200

    Add initial CSS

--- client/src/components/AutoComplete/AutoComplete.js
+++ client/src/components/AutoComplete/AutoComplete.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import MovieIcon from "../../icons/movie.svg";
+import SearchIcon from "../../icons/search.svg";
 import axios from "axios";
 
 const AutoComplete = () => {
@@ -9,7 +11,6 @@ const AutoComplete = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       if (query.length >= 3) {
-        console.log("wtf");
         axios
           .get(
             `https://api.themoviedb.org/3/search/movie?api_key=1b5726d6f7f5bea9a47bce98c8d55b13&language=en-US&query=${query}`
@@ -27,29 +28,37 @@ const AutoComplete = () => {
   }, [query]);
 
   return (
-    <div className="container">
-      <input
-        placeholder="Enter a movie name"
-        onChange={(e) => {
-          setQuery(e.target.value);
-          setLoading(true);
-        }}
-        value={query}
-      ></input>
-      <button>Ieskoti</button>
+    <div className="autocomplete">
+      <div className="autocomplete-search-field">
+        <img src={MovieIcon} alt="Movie icon" />
+        <input
+          placeholder="Enter a movie name"
+          onChange={(e) => {
+            setQuery(e.target.value);
+            setLoading(e.target.value.length >= 3);
+          }}
+          value={query}
+        ></input>
+        <button>
+          <img src={SearchIcon} alt="Search icon" />
+        </button>
+      </div>
+
       {isLoading ? (
         <h1>Loading...</h1>
       ) : (
-        <ul>
-          {movies.map((movie) => {
-            return (
-              <li>
-                <h4>{movie.original_title}</h4>
-                <p>{movie.overview}</p>
-              </li>
-            );
-          })}
-        </ul>
+        <div>
+          <ul>
+            {movies.map((movie) => {
+              return (
+                <li>
+                  <h4>{movie.original_title}</h4>
+                  <p>{movie.overview}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       )}
     </div>
   );
